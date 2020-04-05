04db9c8f6adf3ba52250f08056756f9c45e389b0
jayashankar <jayasankarrnair@gmail.com>
2020-04-05 20:43:11 +0530

    Locked frame rate to 60 in the game

--- RpgGame/Src/main.cpp
+++ RpgGame/Src/main.cpp
@@ -4,13 +4,28 @@ int main(int argc, char* argv[])
 {
 	Game *game = new Game();
 
+	const int frameRate = 60;
+	const int frameDelay = 1000 / 60;
+
+	Uint32 frameStartTime = 0;
+	int frameTime = 0;
+
 	game->Init("RPGGame", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
 	while(game->IsRunning())
 	{
+		frameStartTime = SDL_GetTicks();
+
 		game->HandleEvents();
 		game->Update();
 		game->Draw();
+
+		frameTime = SDL_GetTicks() - frameStartTime;
+
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->clean();
