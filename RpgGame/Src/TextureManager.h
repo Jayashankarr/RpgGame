410963516654665e691399398fb01dbe91211535
jayashankar <jayasankarrnair@gmail.com>
2020-04-06 12:06:18 +0530

    Bug fix

--- RpgGame/Src/TextureManager.h
+++ RpgGame/Src/TextureManager.h
@@ -1,14 +1,10 @@
 #pragma once
-#include "SDL.h"
-#include "SDL_image.h"
+
 #include "Game.h"
 
-class TextureManager
-{
+class TextureManager {
+
 public:
-	
-	static SDL_Texture* CreateTexture(const char* filePath);
-
-private:
-
+	static SDL_Texture* LoadTexture(const char* fileName);
+	static void Draw(SDL_Texture* tex, SDL_Rect src, SDL_Rect dest, SDL_RendererFlip flip);
 };
