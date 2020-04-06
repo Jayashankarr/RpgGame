728961d5969cc87fa676a8d89f9ad43594246eeb
jayashankar <jayasankarrnair@gmail.com>
2020-04-06 12:07:58 +0530

    chenged the project setting

--- RpgGame/Src/main.cpp
+++ RpgGame/Src/main.cpp
@@ -1,27 +1,30 @@
 #include "Game.h"
 
-int main(int argc, char* argv[])
+Game *game = nullptr;
+
+int main(int argc, char *argv[])
 {
-	Game *game = new Game();
 
-	const int frameRate = 60;
-	const int frameDelay = 1000 / 60;
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
 
-	Uint32 frameStartTime = 0;
-	int frameTime = 0;
+	Uint32 frameStart;
+	int frameTime;
 
-	game->Init("RPGGame", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+	game = new Game();
+	game->init("GameWindow", 800, 640, false);
 
-	while(game->IsRunning())
+	while (game->running())
 	{
-		frameStartTime = SDL_GetTicks();
 
-		game->HandleEvents();
-		game->Update();
-		game->Draw();
+		frameStart = SDL_GetTicks();
 
-		frameTime = SDL_GetTicks() - frameStartTime;
+		game->handleEvents();
+		game->update();
+		game->render();
 
+		frameTime = SDL_GetTicks() - frameStart;
+		
 		if (frameDelay > frameTime)
 		{
 			SDL_Delay(frameDelay - frameTime);
@@ -29,6 +32,5 @@ int main(int argc, char* argv[])
 	}
 
 	game->clean();
-
 	return 0;
 }
