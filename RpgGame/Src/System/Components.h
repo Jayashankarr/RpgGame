4985cffdec1cf5abb4083377a5bc0d0902ca9664
jayashankar <jayasankarrnair@gmail.com>
2020-04-06 12:03:14 +0530

    Added all the components required

--- RpgGame/Src/System/Components.h
+++ RpgGame/Src/System/Components.h
@@ -1,3 +1,9 @@
 #pragma once
 #include "EntityComponent.h"
+#include "TransformComponent.h"
 #include "SpriteComponent.h"
+#include "KeyboardController.h"
+#include "ColliderComponent.h"
+#include "TileComponent.h"
+#include "ProjectileComponent.h"
+#include "UILabel.h"
